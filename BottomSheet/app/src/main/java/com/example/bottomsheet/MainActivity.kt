d099b0f853e6358412fd1f4c6ff55e6b28a3551d
Diego Nunez <diego.nunezh@ingenieria.unam.edu>
2023-07-25 14:28:47 -0600

    BottomSheet finished

--- BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
+++ BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.bottomsheet.dialogs.MyBottomSheetDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MyBottomSheetDialog.BottomSheetListener {
+    private lateinit var openDialogButton: Button
+    private lateinit var textViewButton: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        openDialogButton = findViewById(R.id.button_open_dialog)
+        textViewButton = findViewById(R.id.textview)
+
+        openDialogButton.setOnClickListener {
+            val myBottomSheetDialog = MyBottomSheetDialog()
+            myBottomSheetDialog.show(supportFragmentManager, "Dialog")
+        }
     }
-} 
+
+    override fun onButtonClicked(input: String) {
+        textViewButton.text = input
+    }
+}
