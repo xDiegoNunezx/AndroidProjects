118e0982d91d707cf6000f8cc54e2805ff581d7d
Diego Nunez <diego.nunezh@ingenieria.unam.edu>
2023-07-27 11:22:15 -0600

    Collapsing toolbar created

--- CollapsingToolbar/app/src/main/java/com/example/collapsingtoolbar/MainActivity.kt
+++ CollapsingToolbar/app/src/main/java/com/example/collapsingtoolbar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.collapsingtoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.example_menu, menu)
+        return true
     }
 }
