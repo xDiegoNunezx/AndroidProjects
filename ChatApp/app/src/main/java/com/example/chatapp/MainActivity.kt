6519c91e7e7d5cd22eb18f6e143187c926bc4ae8
Diego Nunez <diego.nunezh@ingenieria.unam.edu>
2023-08-30 09:15:34 -0600

    ChatApp updated

--- ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
+++ ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -13,7 +13,13 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.model.User
+import com.example.chatapp.ui.ChatActivity
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import java.net.URI
 
@@ -22,11 +28,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var getResult: ActivityResultLauncher<Intent>
     private val STORAGE_REQUEST_CODE = 3232
     private lateinit var uri: Uri
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val usersRef: CollectionReference = db.collection("Notebook")
     private lateinit var storageRef: StorageReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        storageRef = FirebaseStorage.getInstance().reference
 
         mBinding.signInButton.setOnClickListener {
             signIn()
@@ -57,6 +66,7 @@ class MainActivity : AppCompatActivity() {
         getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
             if(it.resultCode == RESULT_OK) {
                 mBinding.profileImage.setImageURI(it.data?.data)
+                uri = it.data?.data!!
             }
         }
     }
@@ -138,10 +148,22 @@ class MainActivity : AppCompatActivity() {
             val filePath = storageRef.child("profile_images")
                 .child(uri.lastPathSegment!!)
             filePath.putFile(uri).addOnSuccessListener { task ->
-                val result: Task
+                val result: Task<Uri> = task.metadata?.reference?.downloadUrl!!
+                result.addOnSuccessListener {
+                    uri = it
+                }
+                val user = User(userName,uri.toString(),FirebaseAuth.getInstance().currentUser?.uid!!)
+                usersRef.document()
+                    .set(user)
+                    .addOnSuccessListener {
+                        Toast.makeText(this@MainActivity,"Account created",Toast.LENGTH_LONG).show()
+                        sendToAct()
+                    }.addOnFailureListener {
+                        Toast.makeText(this@MainActivity,"Account wasn't created",Toast.LENGTH_LONG).show()
+                    }
             }
         } else {
-
+-
         }
     }
 
@@ -174,5 +196,9 @@ class MainActivity : AppCompatActivity() {
         mBinding.flipper.setInAnimation(this,R.anim.slide_in_right)
         mBinding.flipper.setOutAnimation(this,R.anim.slide_out_left)
         mBinding.flipper.showPrevious()
+    }
+
+    private fun sendToAct() {
+        startActivity(Intent(this@MainActivity, ChatActivity::class.java))
     }
 }
